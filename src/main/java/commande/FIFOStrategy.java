b6128806304062bd474a0e9a844e45f649c3dd8e
lucco <luc.bobo@etu.univ-amu.fr>
2020-10-06 18:12:09 +0200

    Strategy

---
+++ src/main/java/commande/FIFOStrategy.java
@@ -0,0 +1,12 @@
+package commande;
+
+import java.util.List;
+
+public class FIFOStrategy {
+    public void execute (List<Integer> fifo) {
+        for (Integer integer: fifo) {
+            // Ascenseur.goto(integer);
+            // fifo.remove(integer);
+        }
+    }
+}
