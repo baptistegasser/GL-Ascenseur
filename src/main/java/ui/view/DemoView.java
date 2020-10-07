21549ac36083028a9ef597a135c33df9452fc834
Baptiste Gasser <baptistegasser36@gmail.com>
2020-10-07 22:35:54 +0200

    Moved Baptiste A code to ui package, cleaned and refractored

--- src/main/java/ui/view/DemoView.java
+++ src/main/java/ui/view/DemoView.java
@@ -1,8 +1,15 @@
 package ui.view;
 
+import gui.GuiButtonInElevator;
+import ui.DemoApp;
 import ui.controller.DemoController;
+import ui.view.component.ElevatorInsidePanel;
+import ui.view.component.ElevatorStatePanel;
+import ui.view.component.FloorAndProgressPanel;
 
 import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
 
 /**
  * La vue de démonstration représentant de manière schématisé un ascenseur.
@@ -13,6 +20,46 @@ public class DemoView extends JPanel {
      * Le controller lié à cette vue
      */
     private DemoController controller;
+
+    public DemoView(int nbFloor) {
+        super(new GridBagLayout());
+
+        // Contrainte appliqué sur le layout
+        GridBagConstraints constraints = new GridBagConstraints();
+
+        //Place l'élément à la case 0,0
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+
+        //Applique un padding sur l'élément (Surement à revoir) TODO
+        constraints.insets = new Insets(0,0,0,0);
+
+        //Taille de l'élèment
+        constraints.ipady = 100;
+        constraints.ipadx = 100;
+
+        //Applique un élèment avec les contrainte appliqué
+        add(new ElevatorStatePanel(), constraints);
+
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        constraints.insets = new Insets(0,150,20,0);
+        constraints.ipady = 175;
+        constraints.ipadx = 175;
+        add(new ElevatorInsidePanel(nbFloor), constraints);
+
+
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        constraints.insets = new Insets(0,0,0,0);
+
+        //Permet ed centré l'élément su bas, il prend toute la ligne (les 2 colonnes pour être précis)
+        constraints.gridwidth = 2;
+        constraints.ipady = 190;
+        constraints.ipadx = DemoApp.WINDOW_WIDTH;
+        FloorAndProgressPanel panel = new FloorAndProgressPanel(nbFloor);
+        add(panel, constraints);
+    }
 
     public void setController(DemoController controller) {
         this.controller = controller;
---
+++ src/main/java/ui/view/component/ElevatorInsidePanel.java
@@ -0,0 +1,98 @@
+package ui.view.component;
+
+import utils.Utils;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * Ce composant contient l'affichage des boutons présent à l'intérieur de l'ascenseur
+ */
+public class ElevatorInsidePanel extends JPanel {
+    public ElevatorInsidePanel(int nbFloor) {
+        super(new GridLayout(0, 3));
+
+        JButton rdc = new JButton("RDC");
+        rdc.addActionListener(e -> {
+            handleFloorRequest(0);
+        });
+        this.add(rdc);
+        this.add(new JPanel());
+
+        ImageIcon imageIcon;
+        try {
+            URL url = Utils.getResource("notification.png");
+            Image img = ImageIO.read(url).getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+            imageIcon = new ImageIcon(img);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        JButton buttonUrgency = new JButton(imageIcon);
+        buttonUrgency.addActionListener(e -> showEmergencyPopUp());
+
+        this.add(buttonUrgency);
+
+        for (int i = 1; i <= nbFloor; ++i) {
+            JButton button = new JButton();
+
+            final int floor = i;
+            button.addActionListener(e -> {
+                handleFloorRequest(floor);
+            });
+
+            if (i == 0) button.setText("RDC");
+            else button.setText("" + (i));
+            this.add(button);
+        }
+    }
+
+    /**
+     * Affiche une pop up qui bloque l'affichage jusqu'à ce qu'on la ferme
+     */
+    private void showEmergencyPopUp() {
+        final String[] option = {"Retour à la normal"};
+        JOptionPane.showOptionDialog(SwingUtilities.getWindowAncestor(this),
+                "Arrêt d'urgence !",
+                "Attention",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.WARNING_MESSAGE,
+                null,
+                option,
+                option[0]
+        );
+        // TODO faire quelque chose ici quand on ferme
+    }
+
+    /**
+     * Fonction appeler lors du click sur le bouton d'arrêt d'urgence.
+     */
+    private void handleEmergencyStop() {
+        System.out.println("Emergency called");
+    }
+
+    /**
+     * Fonction appeler lors du click sur un bouton d'étage.
+     *
+     * @param floor l'étage lié au bouton
+     */
+    private void handleFloorRequest(int floor) {
+        System.out.println("user want floor " + floor);
+    }
+
+    /**
+     * Change la couleur d'un composant en en orange ou la retire.
+     *
+     * @param on si vrai ajouter la couleur sinon la retirer
+     * @param c le composant a colorier
+     */
+    private void setBacklight(boolean on, Component c) {
+        if (on) {
+            c.setBackground(Color.ORANGE);
+        } else {
+            c.setBackground(null);
+        }
+    }
+}
