1d6ad04e7233d5771d729e67aa91fb08b07f6073
Baptiste <baptiste.arnoulet@etu.univ-amu.fr>
2020-10-07 13:29:57 +0200

    Add ElevatorSimulator

---
+++ src/main/java/simulator/ElevatorSimulator.java
@@ -0,0 +1,13 @@
+package simulator;
+
+public class ElevatorSimulator {
+    int floorCount;
+    int speed;
+    double position;
+
+    public ElevatorSimulator(int floorCount, int speed, double position) {
+        this.floorCount = floorCount;
+        this.speed = speed;
+        this.position = position;
+    }
+}
