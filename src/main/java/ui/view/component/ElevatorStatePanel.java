5d19b05caa645180ff676e21d1b33af85b6c4fd5
Baptiste Gasser <baptistegasser36@gmail.com>
2020-10-08 23:16:27 +0200

    Ajout fonction pour changer l'état

--- src/main/java/ui/view/component/ElevatorStatePanel.java
+++ src/main/java/ui/view/component/ElevatorStatePanel.java
@@ -1,5 +1,7 @@
 package ui.view.component;
 
+import command.State;
+
 import javax.swing.*;
 
 /**
@@ -9,8 +11,39 @@ public class ElevatorStatePanel extends JPanel {
     private final JLabel stateLbl;
 
     public ElevatorStatePanel() {
-        stateLbl = new JLabel("État de l'ascenseur: Arrêt");
-
+        this.stateLbl = new JLabel();
         this.add(stateLbl);
+
+        setState(State.STOPPED);
+    }
+
+    /**
+     * Met à jour l'état affiché de l'ascenseur.
+     *
+     * @param state le nouvelle état
+     */
+    public void setState(State state) {
+        String display = "État de l'ascenseur: ";
+        switch (state) {
+            case STOPPED:
+                display += "arrêté";
+                break;
+            case EMERGENCY:
+                display += "en arrêt d'urgence";
+                break;
+            case MOVING_UP:
+                display += "en monté";
+                break;
+            case MOVING_DOWN:
+                display += "en descente";
+                break;
+            case MOVING_UP_STOP_NEXT:
+                display += "en descente avec arrêt au prochain étage";
+                break;
+            case MOVING_DOWN_STOP_NEXT:
+                display += "en monté avec arrêt au prochain étage";
+                break;
+        }
+        this.stateLbl.setText(display);
     }
 }
