0cfd1e23dc94ff0d08d7757b9c96e47e240be910
lucco <luc.bobo@etu.univ-amu.fr>
2020-10-08 16:35:43 +0200

    Javadoc des stratégies

--- src/main/java/commande/FIFOStrategy.java
+++ src/main/java/commande/FIFOStrategy.java
@@ -4,7 +4,15 @@ import commande.request.Request;
 
 import java.util.ArrayList;
 
+/**
+ * Cette classe permet de choisir les retraites à traiter grâce à la méthode FIFO (First In First Out).
+ * Cette méthode est très simple mais n'est pas du tout optimisée
+ */
 public class FIFOStrategy implements SatisfactionStrategy{
+    /**
+     * Cette méthode renvoie la première requête de la liste des requêtes
+     * Si la liste est vide, elle retourne null
+     */
     @Override
     public Request nextRequest(ArrayList<Request> listRequest) {
         if (listRequest.size() > 0) {
