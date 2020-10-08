75ac50eee5a3f42a8fd7316a4dd6cdefdba435a0
Baptiste Gasser <baptistegasser36@gmail.com>
2020-10-08 21:05:03 +0200

    Réorganisation et documentation des tests du simulateur

--- src/test/java/simulator/ElevatorRemakeTest.java
+++ src/test/java/simulator/ElevatorRemakeTest.java
@@ -24,6 +24,30 @@ class ElevatorRemakeTest {
         simulator.stop();
     }
 
+    /**
+     * Test de la méthode start qui ne doit pas changer l'état de l'ascenseur.
+     */
+    @Test
+    void start() throws InterruptedException {
+        ElevatorModel save = simulator.getModel();
+        simulator.start();
+        Thread.sleep(500);
+        assertEquals(save, simulator.getModel());
+    }
+
+    /**
+     * Vérification que la méthode stop fonctionne.
+     */
+    @Test
+    void stop() throws InterruptedException {
+        simulator.start();
+        Thread.sleep(500);
+        assertDoesNotThrow(() -> simulator.stop());
+    }
+
+    /**
+     * Test le changement d'état.
+     */
     @Test
     void setState() {
         // Check le model de base
@@ -36,6 +60,9 @@ class ElevatorRemakeTest {
         assertEquals(State.MOVING_UP, model.state);
     }
 
+    /**
+     * Test le changement d'état lorsque le simulateur est en marche.
+     */
     @Test
     void setStateWhenRunning() {
         ElevatorModel model = simulator.getModel();
@@ -49,6 +76,9 @@ class ElevatorRemakeTest {
         assertEquals(State.STOPPED, model.state);
     }
 
+    /**
+     * Test la montée de l'ascenseur.
+     */
     @Test
     void assertGoUp() throws InterruptedException {
         ElevatorModel model = simulator.getModel();
@@ -62,83 +92,9 @@ class ElevatorRemakeTest {
         assertTrue(0.9 < model.getPosition() && 1.1 > model.getPosition(), "Position is out of range : " + model.getPosition());
     }
 
-    @Test
-    void assertGoDown() throws InterruptedException {
-        ElevatorModel model = simulator.getModel();
-        model.setPosition(5);
-        assertEquals(5, model.getPosition());
-
-        simulator.start();
-        simulator.setState(State.MOVING_DOWN);
-        Thread.sleep(1000);
-        assertTrue(4.475 < model.getPosition() && 4.505 > model.getPosition(), "Position is out of range : " + model.getPosition());
-        Thread.sleep(1000);
-        assertTrue(3.9 < model.getPosition() && 4.1 > model.getPosition(), "Position is out of range : " + model.getPosition());
-
-        /**
-         * - Arrêt auto au derniers étages où arrêt d'urgence
-         * - Arrêt auto si arrêt au prochain puis passage en mode stoppé
-         */
-    }
-
-    @Test
-    void assertEmergencyStopIfTooLow() throws InterruptedException {
-        ElevatorModel model = simulator.getModel();
-        model.setPosition(0.25);
-
-        simulator.start();
-        simulator.setState(State.MOVING_DOWN);
-        Thread.sleep(750);
-
-        assertEquals(0, model.getPosition());
-        assertEquals(State.EMERGENCY, model.state);
-    }
-
-    @Test
-    void assertEmergencyStopIfTooHigh() throws InterruptedException {
-        ElevatorModel model = simulator.getModel();
-        model.setPosition(7.75);
-
-        simulator.start();
-        simulator.setState(State.MOVING_UP);
-        Thread.sleep(750);
-
-        assertEquals(8.0, model.getPosition());
-        assertEquals(State.EMERGENCY, model.state);
-    }
-
-    @Test
-    void assertStopDontMove() throws InterruptedException {
-        ElevatorModel model = simulator.getModel();
-        model.setPosition(5);
-        assertEquals(5, model.getPosition());
-
-        simulator.start();
-        simulator.setState(State.MOVING_DOWN);
-        Thread.sleep(500);
-
-        simulator.setState(State.STOPPED);
-        double pos = simulator.getModel().position;
-        Thread.sleep(500);
-        assertEquals(pos, simulator.getModel().position);
-    }
-
-    @Test
-    void willStopWillGoingDown() throws InterruptedException {
-        ElevatorModel model = simulator.getModel();
-        model.setPosition(4);
-
-        simulator.start();
-        simulator.setState(State.MOVING_DOWN);
-        Thread.sleep(100);
-
-        simulator.setState(State.MOVING_DOWN_STOP_NEXT);
-        Thread.sleep(2100);
-
-        assertEquals(3, model.position);
-        assertEquals(State.STOPPED, model.state);
-    }
-
+    /**
+     * S'assure que si l'ascenseur monte et doit s'arrêter au prochain étage qu'il le s'arrête.
+     */
     @Test
     void willStopWillGoingUp() throws InterruptedException {
         ElevatorModel model = simulator.getModel();
@@ -155,18 +111,90 @@ class ElevatorRemakeTest {
         assertEquals(State.STOPPED, model.state);
     }
 
+    /**
+     * S'assure que si l'ascenseur monte trop haut, il s'arrête en urgence.
+     */
     @Test
-    void start() throws InterruptedException {
-        ElevatorModel save = simulator.getModel();
+    void assertEmergencyStopIfTooHigh() throws InterruptedException {
+        ElevatorModel model = simulator.getModel();
+        model.setPosition(7.75);
+
         simulator.start();
-        Thread.sleep(500);
-        assertEquals(save, simulator.getModel());
+        simulator.setState(State.MOVING_UP);
+        Thread.sleep(750);
+
+        assertEquals(8.0, model.getPosition());
+        assertEquals(State.EMERGENCY, model.state);
     }
 
+    /**
+     * Test la descente de l'ascenseur.
+     */
     @Test
-    void stop() throws InterruptedException {
+    void assertGoDown() throws InterruptedException {
+        ElevatorModel model = simulator.getModel();
+        model.setPosition(5);
+        assertEquals(5, model.getPosition());
+
         simulator.start();
+        simulator.setState(State.MOVING_DOWN);
+        Thread.sleep(1000);
+        assertTrue(4.475 < model.getPosition() && 4.505 > model.getPosition(), "Position is out of range : " + model.getPosition());
+        Thread.sleep(1000);
+        assertTrue(3.9 < model.getPosition() && 4.1 > model.getPosition(), "Position is out of range : " + model.getPosition());
+    }
+
+    /**
+     * S'assure que si l'ascenseur descend et doit s'arrêter au prochain étage qu'il le s'arrête.
+     */
+    @Test
+    void willStopWillGoingDown() throws InterruptedException {
+        ElevatorModel model = simulator.getModel();
+        model.setPosition(4);
+
+        simulator.start();
+        simulator.setState(State.MOVING_DOWN);
+        Thread.sleep(100);
+
+        simulator.setState(State.MOVING_DOWN_STOP_NEXT);
+        Thread.sleep(2100);
+
+        assertEquals(3, model.position);
+        assertEquals(State.STOPPED, model.state);
+    }
+
+    /**
+     * S'assure que si l'ascenseur descend trop bas, il s'arrête en urgence.
+     */
+    @Test
+    void assertEmergencyStopIfTooLow() throws InterruptedException {
+        ElevatorModel model = simulator.getModel();
+        model.setPosition(0.25);
+
+        simulator.start();
+        simulator.setState(State.MOVING_DOWN);
+        Thread.sleep(750);
+
+        assertEquals(0, model.getPosition());
+        assertEquals(State.EMERGENCY, model.state);
+    }
+
+    /**
+     * Test de l'arrêt de l'ascenseur.
+     */
+    @Test
+    void assertStopDontMove() throws InterruptedException {
+        ElevatorModel model = simulator.getModel();
+        model.setPosition(5);
+        assertEquals(5, model.getPosition());
+
+        simulator.start();
+        simulator.setState(State.MOVING_DOWN);
         Thread.sleep(500);
-        assertDoesNotThrow(() -> simulator.stop());
+
+        simulator.setState(State.STOPPED);
+        double pos = simulator.getModel().position;
+        Thread.sleep(500);
+        assertEquals(pos, simulator.getModel().position);
     }
 }
