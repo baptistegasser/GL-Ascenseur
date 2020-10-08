ebe352ebded6b408bbff8f1dab0f70d5099e3423
Baptiste <baptiste.arnoulet@etu.univ-amu.fr>
2020-10-08 23:49:33 +0200

    Add Test for ControlCommand

--- src/test/java/command/ControlCommandTest.java
+++ src/test/java/command/ControlCommandTest.java
@@ -1,43 +1,90 @@
 package command;
 
-import command.ControlCommand;
-import command.FIFOStrategy;
 import command.request.Request;
 import command.request.RequestType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import simulator.ElevatorRemake;
-import simulator.ElevatorSimulator;
-import ui.model.ElevatorModel;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ControlCommandTest {
 
-    @Test
-    public void addRequestTest() {
-        ElevatorRemake simulator = new ElevatorRemake(6,10);
-        ControlCommand controlCommand = new ControlCommand(simulator, new FIFOStrategy());
+    ElevatorRemake simulator;
+    ControlCommand controlCommand;
 
-
+    @BeforeEach
+    void setUp() {
+        simulator = new ElevatorRemake(6,3);
+        controlCommand = new ControlCommand(simulator, new FIFOStrategy());
     }
 
+    @AfterEach
+    void tearDown() throws InterruptedException {
+        controlCommand.stop();
+        simulator.stop();
+    }
+
+    /**
+     * Vérification que la méthode stop fonctionne.
+     */
     @Test
-    public void actionTest() {
-        try {
-            ControlCommand controlCommand = new ControlCommand(new ElevatorRemake(6, 10), new FIFOStrategy());
+    void stop() throws InterruptedException {
+        controlCommand.start();
+        Thread.sleep(500);
+        assertDoesNotThrow(() -> controlCommand.stop());
+    }
 
-            Request request = new Request(RequestType.GO_TO, 5);
-            Request request1 = new Request(RequestType.OUTSIDE_UP, 2);
-            Request request2 = new Request(RequestType.OUTSIDE_DOWN, 3);
-            Request request3 = new Request(RequestType.OUTSIDE_UP, 4);
+    /**
+     * Verification de ma méthode addRequest dans le cas des requêtes liées aux états d'urgence
+     */
+    @Test
+    public void addRequestEmergencyTest() {
+        Request request = new Request(RequestType.URGENCY, -1);
+        Request request1 = new Request(RequestType.STOP_URGENCY, -1);
 
-            controlCommand.setCurrentRequest(request);
-            controlCommand.getListRequest().add(request);
-            controlCommand.getListRequest().add(request1);
-            controlCommand.getListRequest().add(request2);
-            controlCommand.getListRequest().add(request3);
+        controlCommand.addRequest(request);
+        assertEquals(simulator.getModel().getState(), State.EMERGENCY);
+        assertEquals(controlCommand.getStateInEmergency(), State.STOPPED);
 
-            //controlCommand.action();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        controlCommand.addRequest(request1);
+        assertEquals(simulator.getModel().getState(), State.STOPPED);
+        assertEquals(controlCommand.getStateInEmergency(), State.STOPPED);
+
+        simulator.start();
+        simulator.setState(State.MOVING_UP);
+
+        controlCommand.addRequest(request);
+        assertEquals(simulator.getModel().getState(), State.EMERGENCY);
+        assertEquals(controlCommand.getStateInEmergency(), State.MOVING_UP);
+
+        controlCommand.addRequest(request1);
+        assertEquals(simulator.getModel().getState(), State.MOVING_UP);
+        assertEquals(controlCommand.getStateInEmergency(), State.STOPPED);
+
+        simulator.setState(State.STOPPED);
+    }
+
+    /**
+     * Verification de ma méthode addRequest
+     */
+    @Test
+    public void addRequestTest() {
+        Request request = new Request(RequestType.GO_TO, 3);
+        Request request1 = new Request(RequestType.GO_TO, 4);
+
+        controlCommand.addRequest(null);
+        assertNull(controlCommand.getCurrentRequest());
+        assertEquals(controlCommand.getListRequest().size(), 0);
+
+        controlCommand.addRequest(request);
+        assertEquals(controlCommand.getCurrentRequest(), request);
+        assertEquals(controlCommand.getListRequest().get(0), request);
+
+        controlCommand.addRequest(request1);
+        assertEquals(controlCommand.getCurrentRequest(), request);
+        assertEquals(controlCommand.getListRequest().get(0), request);
+        assertEquals(controlCommand.getListRequest().get(1), request1);
     }
 }
