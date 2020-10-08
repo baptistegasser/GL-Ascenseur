216c0808dfe41ed58d40d222c700c778dccce491
Baptiste Gasser <baptistegasser36@gmail.com>
2020-10-08 17:55:37 +0200

    Fix "e" dans le nom du packet

--- src/test/java/simulator/ElevatorRemakeTest.java
+++ src/test/java/simulator/ElevatorRemakeTest.java
@@ -1,6 +1,6 @@
 package simulator;
 
-import commande.State;
+import command.State;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
