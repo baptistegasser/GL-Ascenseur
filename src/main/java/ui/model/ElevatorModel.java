f27c133aca5aef30ce18fe1cc44364f354442203
Baptiste Gasser <baptistegasser36@gmail.com>
2020-10-08 14:03:11 +0200

    Fix merci César

--- src/main/java/ui/model/ElevatorModel.java
+++ src/main/java/ui/model/ElevatorModel.java
@@ -9,10 +9,25 @@ import utils.Observable;
  */
 public class ElevatorModel extends Observable<ElevatorModel> {
     public int nbFloor;
-    public static double position;
+    public double position;
     public State state;
 
     public ElevatorModel() {
         position = 0;
+    }
+
+    public void setNbFloor(int nbFloor) {
+        this.nbFloor = nbFloor;
+        notifyObservers();
+    }
+
+    public void setPosition(double position) {
+        this.position = position;
+        notifyObservers();
+    }
+
+    public void setState(State state) {
+        this.state = state;
+        notifyObservers();
     }
 }
